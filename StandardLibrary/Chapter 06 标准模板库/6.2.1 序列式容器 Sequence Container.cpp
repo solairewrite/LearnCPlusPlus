8bf4d982ac2e56dc77d3121b39f5907983b43125
solairewrite <1029509407@qq.com>
2019-08-23 01:12:29 +0800

    6.2.4 关联式数组

--- StandardLibrary/Chapter 06 标准模板库/6.2.1 序列式容器 Sequence Container.cpp
+++ StandardLibrary/Chapter 06 标准模板库/6.2.1 序列式容器 Sequence Container.cpp
@@ -1,3 +1,5 @@
+/* 6.2.1 序列式容器 */
+
 // 示例: stl/vector1.cpp
 // 示例: stl/deque1.cpp
 // 示例: stl/array1.cpp
@@ -6,3 +8,12 @@
 /* 6.2.2 关联式容器 */
 
 // 示例: stl/multiset1.cpp
+
+/* 6.2.3 无序容器 */
+
+// 示例: stl/unordmultiset1.cpp
+// 示例: stl/unordmap1.cpp
+
+/* 6.2.4 关联式数组 */
+
+// 示例: stl/assoarray1.cpp
--- StandardLibrary/Chapter 06 标准模板库/multimap1.cpp
+++ StandardLibrary/Chapter 06 标准模板库/multimap1.cpp
@@ -4,23 +4,23 @@
 
 using namespace std;
 
-int main()
-{
-	multimap<int, string> coll;
-
-	coll = {
-		{5,"tagged"},
-		{2,"a"},
-		{1,"this"},
-		{4,"of"},
-		{6,"strings"},
-		{1,"is"},
-		{3,"multimap"}
-	};
-
-	for (auto elem : coll)
-	{
-		cout << elem.second << " ";
-	}
-	cout << endl;
-}
+//int main()
+//{
+//	multimap<int, string> coll;
+//
+//	coll = {
+//		{5,"tagged"},
+//		{2,"a"},
+//		{1,"this"},
+//		{4,"of"},
+//		{6,"strings"},
+//		{1,"is"},
+//		{3,"multimap"}
+//	};
+//
+//	for (auto elem : coll)
+//	{
+//		cout << elem.second << " ";
+//	}
+//	cout << endl;
+//}
