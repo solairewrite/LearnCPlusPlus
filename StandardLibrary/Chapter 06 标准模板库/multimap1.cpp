d9e689dc91b904e9088fff5f891a4601441c40fb
solairewrite <1029509407@qq.com>
2019-08-22 08:38:36 +0800

    6.2.2 关联式容器

---
+++ StandardLibrary/Chapter 06 标准模板库/multimap1.cpp
@@ -0,0 +1,26 @@
+#include <map>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	multimap<int, string> coll;
+
+	coll = {
+		{5,"tagged"},
+		{2,"a"},
+		{1,"this"},
+		{4,"of"},
+		{6,"strings"},
+		{1,"is"},
+		{3,"multimap"}
+	};
+
+	for (auto elem : coll)
+	{
+		cout << elem.second << " ";
+	}
+	cout << endl;
+}
--- StandardLibrary/Chapter 06 标准模板库/multiset1.cpp
+++ StandardLibrary/Chapter 06 标准模板库/multiset1.cpp
@@ -4,24 +4,24 @@
 
 using namespace std;
 
-int main()
-{
-	multiset<string> cities{
-		"Braunschweig", "Hanover", "Frankfurt", "New York",
-		"Chicago", "Toronto", "Paris", "Frankfurt"
-	};
-
-	for (const auto& elem : cities)
-	{
-		cout << elem << ",  ";
-	}
-	cout << endl;
-
-	cities.insert({ "London", "Munich", "Hanover", "Braunschweig" });
-
-	for (const auto& elem : cities)
-	{
-		cout << elem << ",  ";
-	}
-	cout << endl;
-}
+//int main()
+//{
+//	multiset<string> cities{
+//		"Braunschweig", "Hanover", "Frankfurt", "New York",
+//		"Chicago", "Toronto", "Paris", "Frankfurt"
+//	};
+//
+//	for (const auto& elem : cities)
+//	{
+//		cout << elem << ",  ";
+//	}
+//	cout << endl;
+//
+//	cities.insert({ "London", "Munich", "Hanover", "Braunschweig" });
+//
+//	for (const auto& elem : cities)
+//	{
+//		cout << elem << ",  ";
+//	}
+//	cout << endl;
+//}
