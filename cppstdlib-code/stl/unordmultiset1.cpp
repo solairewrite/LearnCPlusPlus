bf4624a798391ba7e7ca3469da1911be96ff822c
solairewrite <1029509407@qq.com>
2019-08-27 23:16:00 +0800

    start 第7章 STL 容器

--- cppstdlib-code/stl/unordmultiset1.cpp
+++ cppstdlib-code/stl/unordmultiset1.cpp
@@ -3,25 +3,25 @@
 #include <iostream>
 using namespace std;
 
-//int main()
-//{
-//    unordered_multiset<string> cities {
-//        "Braunschweig", "Hanover", "Frankfurt", "New York",
-//        "Chicago", "Toronto", "Paris", "Frankfurt"
-//    };
-//    
-//    // print each element:
-//    for (const auto& elem : cities) {
-//        cout << elem << "  ";
-//    }
-//    cout << endl;
-//
-//    // insert additional values:
-//    cities.insert( {"London", "Munich", "Hanover", "Braunschweig"} );
-//
-//    // print each element:
-//    for (const auto& elem : cities) {
-//        cout << elem << "  ";
-//    }
-//    cout << endl;
-//}
+int main()
+{
+    unordered_multiset<string> cities {
+        "Braunschweig", "Hanover", "Frankfurt", "New York",
+        "Chicago", "Toronto", "Paris", "Frankfurt"
+    };
+    
+    // print each element:
+    for (const auto& elem : cities) {
+        cout << elem << "  ";
+    }
+    cout << endl;
+
+    // insert additional values:
+    cities.insert( {"London", "Munich", "Hanover", "Braunschweig"} );
+
+    // print each element:
+    for (const auto& elem : cities) {
+        cout << elem << "  ";
+    }
+    cout << endl;
+}
